fdf7d28c7a155a5b6cabeb4023def83ec2b8a4c0
TheNeetJeet <adamplaxy@gmail.com>
2023-05-16 18:13:26 -0400

    add models

--- src/main/java/com/gaggle/assessment/musicinfo/model/Musician.java
+++ src/main/java/com/gaggle/assessment/musicinfo/model/Musician.java
@@ -4,18 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Getter
 @Setter
-public class Song {
+public class Musician {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Long id;
-    private String isrc;
-    private String title;
-    private String artist;
-    private List<Musician> musicianList;
+    private String name;
+    private String contribution;
 }
