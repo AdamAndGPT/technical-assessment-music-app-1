e4aaf4f84318bfecfdcf758a62f10644d391ae8c
TheNeetJeet <adamplaxy@gmail.com>
2023-05-18 17:20:27 -0400

    get all songs test

--- src/main/java/com/gaggle/assessment/musicinfo/data/DataInitializer.java
+++ src/main/java/com/gaggle/assessment/musicinfo/data/DataInitializer.java
@@ -14,36 +14,42 @@ import java.util.stream.Stream;
 @Component
 @AllArgsConstructor
 public class DataInitializer {
+    public static final String MICHAEL_JACKSON = "Michael Jackson";
+    public static final String JOHN_DENVER = "John Denver";
     private SongRepository songRepository;
 
     public void initData() {
-        songRepository.saveAll(Stream.of(
-                new Song(null, "12345", "Beat It", "Michael Jackson",
+        songRepository.saveAll(getSongStream().collect(Collectors.toList()));
+    }
+
+    public static Stream<Song> getSongStream() {
+        return Stream.of(
+                new Song(null, "12345", "Beat It", MICHAEL_JACKSON,
                         new HashSet<>(Arrays.asList(
-                                new Musician(null, "Michael Jackson", "Singer")))),
+                                new Musician(null, MICHAEL_JACKSON, "Singer")))),
                 new Song(null, "12344", "Thunder Struck", "AC/DC",
                         new HashSet<>(Arrays.asList(
                                 new Musician(null, "Brian Johnson", "Singer"),
                                 new Musician(null, "Angus Young", "Lead Guitar"),
                                 new Musician(null, "Malcom Young", "Rhythm Guitar"),
                                 new Musician(null, "Phil Rudd", "Drummer")))),
-                new Song(null, "12343", "Take Me Home, Country Roads", "John Denver",
+                new Song(null, "12343", "Take Me Home, Country Roads", JOHN_DENVER,
                         new HashSet<>(Arrays.asList(
-                                new Musician(null, "John Denver", "Singer"),
-                                new Musician(null, "John Denver", "Guitar")))),
-                new Song(null, "12342", "Smooth Criminal", "Michael Jackson",
+                                new Musician(null, JOHN_DENVER, "Singer"),
+                                new Musician(null, JOHN_DENVER, "Guitar")))),
+                new Song(null, "12342", "Smooth Criminal", MICHAEL_JACKSON,
                         new HashSet<>(Arrays.asList(
-                                new Musician(null, "Michael Jackson", "Singer")))),
-                new Song(null, "12341", "Billie Jean", "Michael Jackson",
+                                new Musician(null, MICHAEL_JACKSON, "Singer")))),
+                new Song(null, "12341", "Billie Jean", MICHAEL_JACKSON,
                         new HashSet<>(Arrays.asList(
-                                new Musician(null, "Michael Jackson", "Singer")))),
-                new Song(null, "12340", "Bad", "Michael Jackson",
+                                new Musician(null, MICHAEL_JACKSON, "Singer")))),
+                new Song(null, "12340", "Bad", MICHAEL_JACKSON,
                         new HashSet<>(Arrays.asList(
-                                new Musician(null, "Michael Jackson", "Singer")))),
-                new Song(null, "12339", "Chicago", "Michael Jackson",
+                                new Musician(null, MICHAEL_JACKSON, "Singer")))),
+                new Song(null, "12339", "Chicago", MICHAEL_JACKSON,
                         new HashSet<>(Arrays.asList(
-                                new Musician(null, "Michael Jackson", "Singer"))))
+                                new Musician(null, MICHAEL_JACKSON, "Singer"))))
 
-        ).collect(Collectors.toList()));
+        );
     }
 }
---
+++ src/test/java/com/gaggle/assessment/musicinfo/MockMvcTests.java
@@ -0,0 +1,49 @@
+package com.gaggle.assessment.musicinfo;
+
+import com.gaggle.assessment.musicinfo.data.DataInitializer;
+import com.gaggle.assessment.musicinfo.model.Song;
+import com.gaggle.assessment.musicinfo.repository.SongRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.stream.Stream;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = MusicInfoApplication.class)
+@AutoConfigureMockMvc
+class MockMvcTests {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private SongRepository songRepo;
+
+    @Autowired
+    private DataInitializer dataInitializer;
+
+    private Stream<Song> songs = DataInitializer.getSongStream();
+
+    @BeforeEach
+    public void init() {
+        dataInitializer.initData();
+    }
+
+    @Test
+    public void getAllSongs(){
+        ResponseEntity<String> response = restTemplate.getForEntity("/songs", String.class);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+}
